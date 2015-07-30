9893065f8b4f10527144e61986e5dd7aa9a01a19
Ali Paikan <ali.paikan@gmail.com>
2015-07-30 09:32:40 +0200

    init commit

---
+++ adminport.lua
@@ -0,0 +1,139 @@
+#!/usr/bin/lua 
+
+-- Copyright: (C) 2011 Robotics, Brain and Cognitive Sciences - Italian Institute of Technology (IIT)
+-- Author: Ali Paikan <ali.paikan@iit.it>
+-- Copy Policy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+
+
+-- LUA_CPATH should have the path to yarp-lua binding library (i.e. yarp.so, yarp.dll) 
+require("yarp")
+
+-- initialize yarp network
+yarp.Network()
+
+
+--
+-- load a log file
+--
+function load_log(filename)
+  local file = io.open(filename, "r")
+  if file == nil then
+    print("cannot open '"..filename.."'")
+    return nil
+  end
+  data = {}
+  for line in file:lines() do      
+      data[#data + 1] = line
+  end
+  file:close()
+  return data
+end
+
+--
+-- search in a table
+--
+function inTable(tbl, item)
+    for key, value in pairs(tbl) do
+        if value == item then return key end
+    end
+    return nil
+end
+
+--
+-- splits string by a text delimeter
+--
+function string:split(inSplitPattern, outResults)
+   if not outResults then
+      outResults = { }
+   end
+   local theStart = 1
+   local theSplitStart, theSplitEnd = string.find( self, inSplitPattern, theStart )
+   while theSplitStart do
+      table.insert( outResults, string.sub( self, theStart, theSplitStart-1 ) )
+      theStart = theSplitEnd + 1
+      theSplitStart, theSplitEnd = string.find( self, inSplitPattern, theStart )
+   end
+   table.insert( outResults, string.sub( self, theStart ) )
+   return outResults
+end
+
+function help() 
+    local msg = [[
+help            : show help
+exit            : exit portadmin
+load <filename> : loads a connections list file 
+attach <portmonitor> <context> [send|recv] : attach a portmonitor to the list of connections
+]]
+    print(msg)
+end
+
+function attach(cons, plugin, context, side)
+    if side ~= "send" and side ~= "recv" then
+        print("'"..side.."' is not correct. Available options are 'send' and 'recv'.")
+        return false
+    end
+    if cons == nil or #cons == 0 then
+        print("Connections list is empty. Did you load any connection list file?")
+        return false
+    end
+    for i=1,#cons do
+        local ports = cons[i]:split("->")
+        if #ports ~= 2 then
+            print("Error while parsing the connection list file at line "..i)
+            return false
+        end
+        -- triming the spaces
+        src = ports[1]:match "^%s*(.-)%s*$"
+        dest = ports[2]:match "^%s*(.-)%s*$"
+        local ret = yarp.NetworkBase_connect(src, dest,
+                                 "tcp+"..side..".portmonitor+context."..context.."+file."..plugin)
+        if ret == false then
+            print("Cannot connect '"..src.."' to '"..dest.."' using plugin '"..plugin.."'")
+        end
+    end
+    return true
+end
+-------------------------------------------------------
+-- main 
+-------------------------------------------------------
+logo = [[
+                  _            _           _       
+ _ __   ___  _ __| |_ __ _  __| |_ __ ___ (_)_ __  
+| '_ \ / _ \| '__| __/ _` |/ _` | '_ ` _ \| | '_ \ 
+| |_) | (_) | |  | || (_| | (_| | | | | | | | | | |
+| .__/ \___/|_|   \__\__,_|\__,_|_| |_| |_|_|_| |_|
+|_|
+
+type 'help' for more information.
+]]
+
+print(logo)
+
+repeat
+    io.write(">> ") io.flush()
+    local cmd = io.read()
+    if cmd == "exit" or cmd == "quit" then break end
+    tokens = cmd:split(" ")
+    -- loading the file
+    if tokens[1] == "help" then
+        help()
+    elseif tokens[1] == "load" then
+        if #tokens < 2 then 
+            print("Usage: load <filename>.") 
+        else
+            cons = load_log(tokens[2]) 
+        end
+    elseif tokens[1] == "attach" then    
+        if #tokens < 3 then 
+            print("Usage: attach <portmonitor> <context> [send|recv].") 
+        else
+            local side = "recv"
+            if #tokens > 3 then side = tokens[4] end
+            attach(cons, tokens[2], tokens[3], side)
+        end
+    else
+        print("'"..cmd.."' is not correct. type 'help' for more information.")
+    end
+until false
+
+
---
+++ yarpviz.lua
@@ -0,0 +1,201 @@
+#!/usr/bin/lua 
+
+-- Copyright: (C) 2015 iCub Facility - Italian Institute of Technology (IIT)
+-- Author: Ali Paikan <ali.paikan@iit.it>
+-- Copy Policy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+
+
+-- LUA_CPATH should have the path to yarp-lua binding library (i.e. yarp.so, yarp.dll) 
+require("yarp")
+
+-- initialize yarp network
+yarp.Network()
+
+--
+-- splits string by a text delimeter
+--
+function string:split(inSplitPattern, outResults)
+   if not outResults then
+      outResults = { }
+   end
+   local theStart = 1
+   local theSplitStart, theSplitEnd = string.find( self, inSplitPattern, theStart )
+   while theSplitStart do
+      table.insert( outResults, string.sub( self, theStart, theSplitStart-1 ) )
+      theStart = theSplitEnd + 1
+      theSplitStart, theSplitEnd = string.find( self, inSplitPattern, theStart )
+   end
+   table.insert( outResults, string.sub( self, theStart ) )
+   return outResults
+end
+
+--
+--  gets a list of the yarp ports
+--
+function yarp_name_list() 
+  local style = yarp.ContactStyle()
+  style.quiet = true
+  style.timeout = 3.0
+
+  local cmd = yarp.Bottle()
+  local reply = yarp.Bottle()
+  cmd:addString("list")
+  local ret = yarp.NetworkBase_writeToNameServer(cmd, reply, style)
+  if ret == false or reply:size() ~= 1 then
+    print("Error")
+  end
+
+  local str = reply:get(0):asString()
+  local fields = str:split("registration name /")
+  ports_map = {}
+  for i = 1, #fields do
+     local s = fields[i]
+     local name = s:split("ip")
+     name = name[1]:gsub("^%s*(.-)%s*$", "%1")
+     if name ~= "" then
+       name = "/"..name
+       if name ~= yarp.NetworkBase_getNameServerName() then 
+         ports_map[name] = "P"..i
+       end     
+     end     
+  end
+  return ports_map
+end
+
+function get_port_con(port_name)
+  local ping = yarp.Port()  
+  ping:open("/anon_rpc");  
+  ping:setAdminMode(true)
+  local ret = yarp.NetworkBase_connect(ping:getName(), port_name)
+  if ret == false then
+      print("Cannot connect to " .. port_name)
+      return nil
+  end
+  local cmd = yarp.Bottle()
+  local reply = yarp.Bottle()
+  cmd:addString("list")
+  cmd:addString("out")  
+  if ping:write(cmd, reply) == false then
+      print("Cannot write to " .. port_name)
+      ping:close()
+      return nil, nil    
+  end  
+  outs_list = {}
+  for i=0,reply:size()-1 do 
+    outs_list[#outs_list+1] = reply:get(i):asString()
+  end 
+
+  cmd:clear()
+  reply:clear()
+  cmd:addString("list")
+  cmd:addString("in")  
+  if ping:write(cmd, reply) == false then
+      print("Cannot write to " .. port_name)
+      ping:close()
+      return nil, outs_list    
+  end  
+  ins_list = {}
+  for i=0,reply:size()-1 do 
+    ins_list[#ins_list+1] = reply:get(i):asString()
+  end 
+  ping:close()
+  return ins_list, outs_list
+end
+
+---------------------------------------------------------------------
+---  main 
+---------------------------------------------------------------------
+
+param = ""
+for i=1,#arg do
+  param = param .. arg[i] .. " "
+end
+prop = yarp.Property()
+prop:fromArguments(param)
+
+if prop:check("help") then
+  print("Usage: yarpviz.lua [OPTIONS]\n")
+  print("Known values for OPTION are:\n")
+  print("  --out  <output_name>\t Output file name (default: output.txt)")
+  print("  --type <output_type>\t Output type: pdf, eps, svg, jpg, png, txt (default: txt)")
+  print("  --gen  <generator>  \t Graphviz-based graph generator: dot, neato, twopi, circo (default: dot)")
+  print("  --only-cons         \t Shows only the ports with a connections")
+  os.exit()
+end
+
+local ports = yarp_name_list()
+
+typ = "txt"
+if prop:check("type") then typ = prop:find("type"):asString() end
+if typ == "txt" then 
+    -- creating dot file
+    local filename = "output.txt"
+    if prop:check("out") then filename = prop:find("out"):asString() end
+    local file = io.open(filename, "w")
+    if file == nil then
+      print("cannot open", filename)
+      os.exit()
+    end 
+
+    for name,node in pairs(ports) do   
+       print("checking "..name.." ...")
+       local ins, outs = get_port_con(name, "out")
+       for i=1,#outs do       
+         file:write(name.." -> "..outs[i].."\n")
+       end
+    end
+    file:close()
+    os.exit()
+end
+
+-- creating dot file
+local filename = "output."..typ
+if prop:check("out") then filename = prop:find("out"):asString() end
+local file = io.open(filename..".dot", "w")
+if file == nil then
+  print("cannot open", filename..".dot")
+  os.exit()
+end  
+  
+
+dot_header = [[
+digraph "" {
+  graph [ranksep="1.0", nodesp="0.5", rankdir="LR", overlap="false", packmode="graph", fontname="Arial", fontsize="12", concentrate="true", bgcolor="#FFFFFF"];
+  node [style="filled", color="blue", fillcolor="#F0F0F0", label="", sides="4", fontcolor="black", fontname="Arial", fontsize="12", shape="ellipse"];
+  edge [color="#CC0044", label="", fontname="Arial", fontsize="8", fontcolor="#555555"];]]
+  
+file:write(dot_header.."\n")
+
+for name,node in pairs(ports) do 
+    if prop:check("only-cons") == true then
+        local ins, outs = get_port_con(name)
+        if #outs ~= 0 or #ins ~=1 then
+            file:write(node.." [label=\""..name.."\"]\n")
+        end    
+     else
+        file:write(node.." [label=\""..name.."\"]\n")
+    end 
+end
+
+for name,node in pairs(ports) do   
+   print("checking "..name.." ...")
+   local ins, outs = get_port_con(name, "out")
+   for i=1,#outs do       
+     local to = ports[outs[i] ]
+     file:write(node.." -> "..to.."\n")
+   end
+end
+
+file:write("}\n")
+file:close()
+
+
+gen = "dot"
+if prop:check("gen") then gen = prop:find("gen"):asString() end
+typ = "pdf"
+
+-- creating pdf
+os.execute(gen.." -T"..typ.." -o "..filename.." "..filename..".dot")
+
+-- Deinitialize yarp network
+yarp.Network_fini()
