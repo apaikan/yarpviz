1c6d2d5d9a992ccbbf4dbf7f2a5160b0519e2ace
Ali Paikan <ali.paikan@gmail.com>
2015-07-30 14:27:28 +0200

    simple test

---
+++ test/test.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <yarp/os/Network.h>
+#include <yarp/os/BufferedPort.h>
+#include <yarp/os/Bottle.h>
+
+
+using namespace std;
+using namespace yarp::os;
+
+int main(int argc, char *argv[]) {
+    Network yarp;
+
+	struct sched_param sch_param;
+	sch_param.__sched_priority = sched_get_priority_max(SCHED_FIFO) / 4;
+	if( sched_setscheduler(0, SCHED_FIFO, &sch_param) != 0 ) {
+		cout<<"sched_setscheduler failed."<<endl;
+		return 0;
+	}
+
+    cout<<"Current sched policy: '"<<sched_getscheduler(0)<<"' and priority: '"<<sch_param.__sched_priority<<"'\n";
+
+    BufferedPort<Bottle> inPort;
+    if(!inPort.open("/test"))
+        return 0;
+    
+    if(!NetworkBase::connect("/coman/left_arm/state:o", 
+                            inPort.getName(), "udp")) {
+        cout<<"Cannot connect!"<<endl;
+        return 0;
+    }                            
+
+    cout<<"Running...."<<endl;
+    while (true) {
+        Bottle* msg = inPort.read();
+    }
+    return 0;
+}
+
